bd3a7059f72558d06c0fbab9be7fcbe526aba6f4
ChenYJ1016 <41606624+ChenYJ1016@users.noreply.github.com>
2024-08-12 15:08:35 +0800

    Initial Commit

---
+++ wizShop-public-webservice/src/main/java/com/capstone/wizShop_public_webservice/WizShopPublicWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.wizShop_public_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizShopPublicWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizShopPublicWebserviceApplication.class, args);
+	}
+
+}
